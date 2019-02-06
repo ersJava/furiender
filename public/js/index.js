f2124c03e18fa067c2f1238c410f5782a7c79e49
Beth <ersergeant@gmail.com>
2019-02-05 19:53:03 -0500

    testing

--- public/js/index.js
+++ public/js/index.js
@@ -29,8 +29,7 @@ var API = {
     });
   }
 };
-lkjlkjkljkljkljkjj
-// refreshExamples gets new examples from the db and repopulates the list
+
 var refreshExamples = function() {
   API.getExamples().then(function(data) {
     var $examples = data.map(function(example) {
